32d51203dfda25b2398142aeca9888a37e0d03f4
Rocabor <86777744+Rocabor@users.noreply.github.com>
2025-08-03 02:14:43 -0400

    Delete Modulo_2/JavaScript/Tarea_js directory

--- Modulo_2/JavaScript/Tarea_js/Tarea_js/Ejercicio_1.js
+++
@@ -1,8 +0,0 @@
-
-
-let nombre = "Roberto";
-console.log("Nombre de usuario:", nombre);
-let edad = 30;
-console.log("Edad de usuario:", edad);
-let esEstudiante = true;
-console.log("Estudia?:", esEstudiante);
--- Modulo_2/JavaScript/Tarea_js/Tarea_js/Ejercicio_2.js
+++
@@ -1,7 +0,0 @@
-
-
-let nomb=prompt("ingrese su nombre:");
-let añonac=prompt("ingrese su año de nacimiento:");
-const ap=2025;
-let resta = ap-añonac;
-console.log(`Hola ${nomb}, tienes aproximadamente ${resta} años`);
