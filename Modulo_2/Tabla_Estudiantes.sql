5bb2654a589043f8bac2f43f9f142aa462fea7a2
Rocabor <86777744+Rocabor@users.noreply.github.com>
2025-08-08 14:18:51 -0400

    Create Tabla_Estudiantes.sql

---
+++ Modulo_2/Tabla_Estudiantes.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE Estudiante (
+    cedula_n INT PRIMARY KEY,
+    nombre VARCHAR(100),
+  	apellido VARCHAR(100)
+);
+
+INSERT INTO Estudiante (cedula_n, nombre, apellido) VALUES
+('1618354', 'roberto', 'borges'),
+('29767741', 'jinmy', 'chacin'),
+('30604870', 'maria', 'urdaneta'),
+('30820000', 'daniel', 'sandoval'),
+('31279773', 'daviannys', 'villalobos'),
+('31913070', 'humberto', 'alvarez'),
+('32056687', 'german', 'suarez'),
+('32636138', 'juan', 'polania'),
+('32698769', 'jose', 'velasquez'),
+('33319114', 'gabriel', 'suarez'),
+('33607751', 'robert', 'boscan'),
+('33893333', 'carlos', 'moreno'),
+('33966984', 'jinm', 'chacin'),
+('34544638', 'juan', 'arrieta');
