4478c9c947ba323b81ae4f6ee9c0a7a7cb007cc0
Rocabor <86777744+Rocabor@users.noreply.github.com>
2025-08-09 13:53:44 -0400

    Create tabla.sql

---
+++ Modulo_2/SQL/tabla.sql
@@ -0,0 +1,26 @@
+CREATE TABLE canciones (
+    titulo VARCHAR (100),
+    cantante VARCHAR (100),
+    autor VARCHAR (100),
+    origen VARCHAR (100),
+    genero VARCHAR(100),
+    año INT (5)
+
+);
+
+INSERT INTO canciones (titulo, cantante, autor, origen, genero) VALUES
+('El Cantante','Hector Lavoe','Ruben Blades','Puerto Rico','Salsa',1978),
+('Lost on You','Laura Pergolizzi','Laura Pergolizzi','Estados Unidos',' Indie pop',2015),
+('Thriller','Michael Jackson','Rod Temperton','Estados Unidos',' Funk disco',1982),
+('Imagine','Jhon lennon','Jhon Lennon','Reino Unido','Rock/Pop',1971),
+('Vivir mi Vida','Marc Anthony','Marc Anthony','EEUU','Salsa',2013),
+('Bailando','Enrique Iglesias','Enrique Iglesias','España','Pop',2014),
+('Clocks','Coldplay','Chis Martin','Reino Unido','Rock/Pop',2003),
+('Smooth','Santana/Rob Thomas','Santana/Rob thomas','EEUU','Rock/Pop',1999),
+('La Flaca','Jarabe de Palo','Pau Dones','España','Rock/Pop',1996),
+('Despacito','Luis Fonsi','Luis Fonsi/Daddy Yankee','Puerto Rico','Reggaeton',2017),
+('Bohemian Rhapsody','Queen','Freddy Mercury','Reino Unido','Rock Progresivo',1975),
+('Imagine','Jhon lennon','Jhon Lennon','Reino Unido','Rock/Pop',1971),
+('Color Esperanza','Diego Torres','Coti Sorokin/Diego Torres','Argetina','Pop Latino',2001),
+('Corazon Partio','Alejando Sanz','Alejandro Sanz','España','Pop',1997),
+('Rayando el Sol','Mana','Fher Olivera','Mexico','Rock Latino',1990);
